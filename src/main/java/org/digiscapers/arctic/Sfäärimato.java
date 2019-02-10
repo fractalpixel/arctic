5e31ef6755e0b80c3b64fd3d5b7b9e2d9b550f4e
fractalpixel <fractalpixel@tutanota.com>
2019-02-10 22:15:11 +0200

    Added spaceballs

--- src/main/java/org/digiscapers/arctic/Sfäärimato.java
+++ src/main/java/org/digiscapers/arctic/Sfäärimato.java
@@ -2,6 +2,8 @@ package org.digiscapers.arctic;
 
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 /**
  * BONUS STAGE!  Unlock with MAD SKILLS!  This is an EASTER SPHERE!
  *
@@ -18,6 +20,8 @@ import processing.core.PApplet;
 public class Sfäärimato extends PApplet {
 
     long startTime = 0;
+    long lastFrameTime = System.currentTimeMillis();
+    float frameDurationSeconds = 0.001f;
 
     /**
      * Called when program starts
@@ -34,19 +38,21 @@ public class Sfäärimato extends PApplet {
 // UNCOMMENT FOR COMPO VERSION!!
 //        size(1920, 1080, P2D);
         size(1200, 800, P2D);
+        fullScreen();
     }
 
     @Override
     public void setup() {
+        noCursor();
         background(0.5f);
         startTime = millis();
 
         // NOTE: Colors are now given as Hue, Saturation, Brightness, (Alpha),
-        // each float parameter ranging between 0f and 100f!
-        colorMode(HSB, 100f);
+        // each float parameter ranging between 0f and 1f!
+        colorMode(HSB, 1f);
 
         // DEBUG: Jump to desired time
-       // setTimeSeconds(30);
+   //     setTimeSeconds(55);
 
     }
 
@@ -54,11 +60,23 @@ public class Sfäärimato extends PApplet {
     public void draw() {
         clear();
 
+        // Calculate delta time
+        long currentTime = millis();
+        frameDurationSeconds = (currentTime - lastFrameTime) / 1000f;
+        lastFrameTime = currentTime;
+
         float introStart = 0;
         float introEnd = 35;
         float palloStart = introEnd - 6;
         float palloEnd = palloStart + 25;
-        float demoEnd = palloEnd + 1;
+        float spaceStart = palloEnd - 12;
+        float spaceEnd = spaceStart + 50;
+        float demoEnd = spaceEnd + 2;
+
+        // Palloavaruus! (draw under previous effects)
+        if (timeBetween(spaceStart, spaceEnd)) {
+            palloavaruus(getTimeSeconds() - spaceStart, relativeTime(spaceStart, spaceEnd));
+        }
 
         // Häröpallo stage!  (Draw under intro as it fades in from underneath)
         if (timeBetween(palloStart, palloEnd)) {
@@ -69,6 +87,7 @@ public class Sfäärimato extends PApplet {
         if (timeBetween(introStart, introEnd)) {
             introStage(getTimeSeconds() - introStart, relativeTime(introStart, introEnd));
         }
+
 
         // TODO: Add more awesome SFÄÄRIMATO stages!
 
@@ -110,21 +129,21 @@ public class Sfäärimato extends PApplet {
         float tx = disturb * sin(time * 17f);
         float ty = disturb * cos(time * 13f);
         if (time > 15 && time < 30) text("⛯* SFÄÄRIMATO *⛯", width * (0.08f+ tx) - umts*50f+50, height * (0.5f + ty)- umts*50f+50);
-        fill(30, 50, 200);
-        textSize(40);
-        if (time > 17 && time < 25) text("Ja Homeopaattinen Harmonia", width*0.1f, height * 0.7f);
+//        fill(30, 50, 200);
+//        textSize(60);
+//        if (time > 17 && time < 25) text("Ja Homeopaattinen Harmonia", width*0.1f, height * 0.7f);
     }
 
 
-    SinPulsar bump = new SinPulsar(5f, 0.3f, 1f, 3f);
+    SinPulsar bump = new SinPulsar(5f, 0.05f, 1f, 0.5f);
     SinPulsar twister = new SinPulsar(6.7f, 0.4f, 0.01f, 1.6f);
     SinPulsar twistee = new SinPulsar(15.2f, 0.1f, 0.01f, 0.6f);
     SinPulsar xWobble = new SinPulsar(14.3f, 0.03f, 1f, 0.7f, 0.82f);
     SinPulsar yWobble = new SinPulsar(15.7f, 0.03f, 1f, 0.6f, 0.23f);
     SinPulsar colorWobble = new SinPulsar(10.7f, 0.2f, 1f, 1f);
     void häröpalloStage(float seconds, float relativeTime) {
-        // Colors are hue, sat, brightness, alpha, range 0..100f.
-        colorMode(HSB, 100f);
+        // Colors are hue, sat, brightness, alpha, range 0..1f.
+        colorMode(HSB, 1f);
 
         bump.setWaveTime(lerp(5, 1, relativeTime));
         bump.setSharpness(lerp(0.3f, 5f, relativeTime));
@@ -137,16 +156,16 @@ public class Sfäärimato extends PApplet {
         float x = width * 0.5f * xWobble.get();
         float y = height * 0.5f * yWobble.get();
 
-        float hue = 70 * colorWobble.get();
-        float sat = 50 * bump.get();
-        float brightness = 50 * 2 * bump.get();
-        float alpha = lerp(15, 35, relativeTime);
+        float hue = 0.7f * colorWobble.get();
+        float sat = 0.5f * bump.get();
+        float brightness = 0.5f * 2 * bump.get();
+        float alpha = lerp(0.15f, 0.35f, relativeTime);
         fill(hue, sat, brightness, alpha);
         noStroke();
         //ellipse(x, y, r, r);
 
         twistee.setAmplitude(twister.get());
-        float levels = lerp(1.5f, 3.5f, relativeTime);
+        float levels = lerp(1.5f, 4.5f, relativeTime);
         float palloRadius = r * lerp(0.1f, 0.7f, relativeTime);
         if (relativeTime > 0.95) palloRadius *= map(relativeTime, 0.95f, 1, 1, 7f);
         float twistScale = lerp(0.3f, 1.6f, relativeTime);
@@ -156,7 +175,7 @@ public class Sfäärimato extends PApplet {
 
     void häröpallo(float x, float y, float radius, float scale, float twist, float levels, float twistScale, float h, float s, float b, float a) {
 
-        h += 7f; // Wander
+        h += 0.07f; // Wander
         b *= 0.9f; // Darken
         s *= 1.1f;  // Intensify
 
@@ -177,6 +196,65 @@ public class Sfäärimato extends PApplet {
             }
         }
     }
+
+
+    class AvaruusPallo {
+        float r = 0f;
+        float a = random(2f * PI);
+        float z = 0f;
+        float speed = 0.1f + 0.001f * randomGaussian();
+        float hue = 0.7f + 0.05f * randomGaussian();
+        float sat = 0.5f + 0.1f * randomGaussian();
+        float lum = 0.5f + 0.1f * randomGaussian();
+
+        float maxZ = 8f + 1f * randomGaussian();
+
+        void updateAndDraw() {
+            z += frameDurationSeconds * speed;
+            r += frameDurationSeconds * speed * z * 100f; // Crappy projection to radial distance
+
+            // Warp to start when going round
+            if (z > maxZ) {
+                z = 0f;
+                r = 0f;
+            }
+            if (z < -1) z = -1;
+
+            float x = width/2f + r * cos(a);
+            float y = height/2f + r * -sin(a);
+            float size = height * 0.005f * z * z;
+
+            float alpha = fadeInOut(z / maxZ, 0f, 1f, 0f, 0.4f, 0.4f, 0);
+            fill(hue, sat, lum, alpha);
+            ellipse(x, y, size, size);
+        }
+    }
+
+    ArrayList<AvaruusPallo> avaruusPallot = new ArrayList<AvaruusPallo>();
+
+    void palloavaruus(float seconds, float relativeTime) {
+        // Colors are hue, sat, brightness, alpha, range 0..100f.
+        colorMode(HSB, 1f);
+
+        int ballNum = (int) fadeInOut(relativeTime, 0, 200, 200, 0.6f, 0.1f, 0);
+
+        // Adjust number of balls
+        while (avaruusPallot.size() < ballNum) {
+            avaruusPallot.add(new AvaruusPallo());
+        }
+        while (avaruusPallot.size() > ballNum && ballNum > 0) {
+            avaruusPallot.remove(0);
+        }
+
+        float pesukoneEffekti = fadeInOut(relativeTime, 0, 0.4f, -0.8f, 0.35f, 0.25f, 0.4f);
+        float ballSpeed = fadeInOut(relativeTime, 0.05f, 1f, -5f, 0.7f, 0.18f, 0);
+        for (AvaruusPallo avaruusPallo : avaruusPallot) {
+            avaruusPallo.speed = ballSpeed;
+            avaruusPallo.a += pesukoneEffekti * frameDurationSeconds;
+            avaruusPallo.updateAndDraw();
+        }
+    }
+
 
 
     // Utility functions below
@@ -231,6 +309,61 @@ public class Sfäärimato extends PApplet {
      */
     void setTimeSeconds(float demoTime) {
         startTime = (long) (millis() - demoTime * 1000);
+    }
+
+    /**
+     * @param t goes from 0 to 1 and controls the value produced
+     * @param startValue value when t <= 0
+     * @param midValue value after t >= rampLength and while t <= 1 - rampLength
+     * @param endValue value when t >= 1
+     * @param rampLength length of transition from start to mid value and from mid to end value.
+     * @return smoothly interpolated value
+     */
+    float fadeInOut(float t, float startValue, float midValue, float endValue, float rampLength) {
+        return fadeInOut(t, startValue, midValue, endValue, rampLength, rampLength, 0f);
+    }
+
+    /**
+     * @param t goes from 0 to 1 and controls the value produced
+     * @param startValue value when t <= 0
+     * @param midValue value after t >= startRampLength and while t <= 1 - endRampLength
+     * @param endValue value when t >= 1
+     * @param startRampLength length of transition from start to mid value
+     * @param endRampLength length of transition from mid to end value.
+     * @param startDelay t value before starting first ramp
+     * @return smoothly interpolated value
+     */
+    float fadeInOut(float t, float startValue, float midValue, float endValue, float startRampLength, float endRampLength, float startDelay) {
+        if (t <= startDelay) {
+            return startValue;
+        }
+        else if (t <= startDelay + startRampLength) {
+            return smoothInterpolate(startValue, midValue, relativePos(t, startDelay, startDelay + startRampLength));
+        }
+        else if (t > startDelay + startRampLength && t < (1f - endRampLength)) {
+            return midValue;
+        }
+        else {
+            return smoothInterpolate(midValue, endValue, relativePos(t, 1f - endRampLength, 1f));
+        }
+    }
+
+
+    float relativePos(float value, float start, float end) {
+        if (end == start) return 0.5f;
+
+        float t = value - start;
+        return t / (end - start);
+    }
+
+    float smoothInterpolate(float a, float b, float t) {
+        // Clamp
+        if (t < 0f) return a;
+        if (t > 1f) return b;
+
+        //float smoothStepT = t * t * (3f - 2f * t);
+        float t2 = (1f - cos(t*PI)) / 2f;
+        return lerp(a, b, t2);
     }
 
 }
